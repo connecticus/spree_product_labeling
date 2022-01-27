e0bad15d9744908435970c12d3c7dd7c0d56901c
stas <s@connecticus.ru>
2022-01-26 22:27:12 -0600

    last

--- app/overrides/add_labeling_to_product_edit.rb
+++ app/overrides/add_labeling_to_product_edit.rb
@@ -1,11 +1,48 @@
 Deface::Override.new(
-  :virtual_path       => 'spree/admin/products/_form',
-  :name               => 'add_labeling_to_products_edit',
-  :insert_after       => '[data-hook="admin_product_form_description"]',
-  :text               => '<div data-hook="admin_product_form_labeling">
+    :virtual_path => 'spree/admin/products/_form',
+    :name => 'add_labeling_to_products_edit',
+    :insert_after => '[data-hook="admin_product_form_description"]',
+    :text => '
+
+
+<div class=row>
+<div class="col-md-11">
+<div data-hook="admin_product_form_labeling">
                             <%= f.field_container :labeling, class: [\'form-group\'] do %>
                               <%= f.label :labeling, Spree.t(:labeling) %>
-                              <%= f.text_area :labeling, { rows: "6", class: \'form-control\' } %>
+                              <%= f.text_field :labeling, { rows: "6", class: \'form-control\' } %>
                               <%= f.error_message_on :labeling %>
                             <% end %>
-                          </div>')
+                          </div>
+</div>
+<div class="col-md-1">
+
+
+<div id="colorPicker">
+        <a class="color"><div class="colorInner"></div></a>
+        <div class="track"></div>
+        <ul class="dropdown"><li></li></ul>
+                              <%= f.hidden_field :labeling_color, { rows: "6", class: \'colorInput\' } %>
+
+    </div>
+
+</div>
+
+ </div>
+
+<script type="text/javascript">
+		$(document).ready(function()
+        {
+			var $box = $("#colorPicker");
+$box.tinycolorpicker();
+});
+</script>
+
+<style>
+#colorPicker .colorInner {
+background-color: <%=@product.labeling_color%>;
+}
+</style>
+
+
+')
--- db/migrate/20161129112919_add_labeling_to_spree_products.spree_product_labeling.rb
+++ db/migrate/20161129112919_add_labeling_to_spree_products.spree_product_labeling.rb
@@ -1,5 +1,6 @@
-class AddLabelingToSpreeProducts < ActiveRecord::Migration
+class AddLabelingToSpreeProducts < ActiveRecord::Migration[4.2]
   def change
     add_column :spree_products, :labeling, :string
+    add_column :spree_products, :labeling_color, :string
   end
 end
